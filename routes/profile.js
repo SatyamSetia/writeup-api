d25c44e955e8342fb57b5a5f9c7a077f3d743204
SatyamSetia <satyam7991@gmail.com>
2018-11-16 13:50:29 +0530

    Feature: Get profile

--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,52 @@ const route = require('express').Router();
 const { User, UserDetails }  = require('../db/index');
 const { getIdFromToken } = require('../services/jwt');
 
-route.get('/:username', (req, res) => {
-  res.status(200).json({
-    username: req.params.username
+route.get('/:username', async (req, res) => {
+
+  const followingUserDetails = await UserDetails.findOne({
+    where: {
+      username: req.params.username
+    }
   });
+
+  if(req.headers.token) {
+
+    const decryptedToken = getIdFromToken(req.headers.token);
+
+    if(decryptedToken.error) {
+      return res.status(401).json({
+        errors: {
+          message: ["Invalid Token"]
+        }
+      })
+    } else {
+      const followerUser = await User.findByPrimary(decryptedToken.id);
+
+      const followingUser = await User.findByPrimary(followingUserDetails.user_id);
+
+      let isFollowing = await followingUser.hasFollower(followerUser);
+
+      return res.status(200).json({
+        profile: {
+          username: followingUserDetails.dataValues.username,
+          bio: followingUserDetails.dataValues.bio,
+          image: followingUserDetails.dataValues.image,
+          following: isFollowing
+        }
+      })
+    }
+
+  } else {
+    return res.status(200).json({
+      profile: {
+        username: followingUserDetails.dataValues.username,
+        bio: followingUserDetails.dataValues.bio,
+        image: followingUserDetails.dataValues.image,
+        following: false
+      }
+    })
+  }
+
 })
 
 route.post('/:username/follow', async (req, res) => {
